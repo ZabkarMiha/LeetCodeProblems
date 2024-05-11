f31cafe757ec2587e6e2bf8ab3096fdf7913095d
Miha Zabkar <miha@dsmn.com>
2024-05-11 19:39:05 +0200

    Problems I already solved.

---
+++ PalindromeNumber/Program.cs
@@ -0,0 +1,23 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.WriteLine(isPalindrome(101)); 
+
+bool isPalindrome(int x)
+{
+    if (x < 0) return false;
+
+    int xCopy = x;
+    
+    int reversed = 0;
+
+    while (x > 0)
+    {
+        int lastDigit = x % 10;
+        
+        reversed = (reversed * 10) + lastDigit;
+
+        x = x / 10;
+    }
+
+    return xCopy == reversed;
+}
---
+++ TwoSum/Program.cs
@@ -0,0 +1,22 @@
+﻿TwoSum(new int[] { 0, 4, 3, 0 }, 0);
+
+int[] TwoSum(int[] nums, int target)
+{
+    int num1;
+    List<int> solution = new List<int>();
+    for (int i = 0; i < nums.Length - 1; i++)
+    {
+        num1 = nums[i];
+        for (int j = i + 1; j < nums.Length; j++)
+        {
+            if (num1 + nums[j] == target)
+            {
+                solution.Add(i);
+                solution.Add(j);
+            }
+        }
+    }
+
+    Console.WriteLine(solution[0] + " " + solution[1]);
+    return solution.ToArray();
+}
