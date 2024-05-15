c040d19b9f9a4962a75db35823e2872fc597877f
Miha Zabkar <miha@dsmn.com>
2024-05-15 20:28:04 +0200

    LongestCommonPrefix problem solved.

---
+++ LongestCommonPrefix/Program.cs
@@ -0,0 +1,38 @@
+﻿Console.WriteLine(LongestCommonPrefix(["flow","flight"]));
+
+string LongestCommonPrefix(string[] strs)
+{
+    if (strs.Contains("")) return "";
+    
+    if (strs.Length == 1) return strs[0];
+
+    string prefix = strs.First();
+    
+    foreach (var word in strs.Skip(1))
+    {
+        prefix = FindPrefix(prefix, word);
+    }
+
+    return prefix;
+}
+
+string FindPrefix(string one, string two)
+{
+    string prefix = string.Empty;
+
+    if (one is "") return "";
+
+    if (one[0] != two[0]) return "";
+
+    int length = one.Length < two.Length ? one.Length : two.Length;
+
+    for (int i = 0; i < length; i++)
+    {
+        if (one[i] == two[i])
+            prefix += one[i];
+        else
+            break;
+    }
+    
+    return prefix;
+}
